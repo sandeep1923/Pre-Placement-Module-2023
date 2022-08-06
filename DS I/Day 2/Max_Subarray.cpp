1f79981b788f07d80955c376fda60e5e6fbde006
Sandeep Gupta <75659037+sandeep1923@users.noreply.github.com>
2022-08-07 00:31:34 +0530

    Update Max_Subarray.cpp

--- DS I/Day 2/Max_Subarray.cpp
+++ DS I/Day 2/Max_Subarray.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+    
+public:
+    
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum = nums[0] , ans = nums[0] ;
+        int n = nums.size() ;
+        
+        for(int i=1;i<n;i++)
+        {
+            sum+=nums[i] ;
+    
+            if( sum < nums[i] )
+            {
+                sum = max(sum,nums[i]) ;
+            }
+            
+            ans = max(ans,sum) ;
+        }
+        
+        return ans ;
+    }
+    
+};
